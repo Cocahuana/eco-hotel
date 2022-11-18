053af654e085821a30ca51369120520535a585f5
Cocahuana <ezequieldominguez11@gmail.com>
2022-11-18 15:21:29 -0300

    Feat: Svg imgs set to accent1 color with hex

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import Hero from "../../components/Hero/Hero";
 import ecoHotelImg from "../../assets/img/ecoHotel.jpg";
-import helicopter from "../../assets/img/ecoHotelOutside-helicopter.jpg";
 import HeroContent from "./components/HeroContent";
 import ValueProposal from "./components/ValueProposal/ValueProposal";
 const ecoHotel = {
@@ -19,12 +18,14 @@ function Home() {
 					position='right'
 				/>
 			</Hero>
-			<Hero
-				img={helicopter}
-				alt='Eco Hotel'
-				bg='black'
-				position='left'
-			></Hero>
+			<Hero img={ecoHotelImg} alt='Eco Hotel' bg='white' position='left'>
+				<HeroContent
+					title={ecoHotel.title}
+					subTitle={ecoHotel.subTitle}
+					description={ecoHotel.description}
+					position='left'
+				/>
+			</Hero>
 			<ValueProposal />
 		</>
 	);
--- src/Pages/Home/components/ValueProposal/ValueProposal.jsx
+++ src/Pages/Home/components/ValueProposal/ValueProposal.jsx
@@ -1,17 +1,8 @@
-import {
-	Heading,
-	Container,
-	Flex,
-	Button,
-	Text,
-	Box,
-	Stack,
-	Icon,
-} from "@chakra-ui/react";
-import buildingIcon from "../../../../assets/svg/building-icon.png";
-import qualityIcon from "../../../../assets/svg/quality-icon.png";
+import { Heading, Flex, Text, Box } from "@chakra-ui/react";
+import buildingIcon from "./building-icon.svg";
+import qualityIcon from "./quality-icon.svg";
 import GoldHeading from "../../../../components/Heading/GoldHeading";
-import Card from "./Card";
+import CustomCard from "./CustomCard";
 import CTAGold from "../../../../components/Buttons/CTAGold";
 function ValueProposal() {
 	const proposal = {
@@ -19,9 +10,21 @@ function ValueProposal() {
 		subTitle: "EL UNICO CONSTRUIDO",
 		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a nunc lacus. Suspendisse semper diam nec porta ornare. Morbi id interdum risus. Maecenas laoreet magna tellus, in varius tellus congue sit amet. Vivamus condimentum quis magna id egestas. Morbi posuere quam urna, nec porta sem ultrices sed. Integer in hendrerit quam, vel iaculis nunc. Morbi ac diam sed lectus aliquam dictum. Nulla mauris tortor, condimentum eu augue quis, finibus pellentesque ante. In hac habitasse platea dictumst. Cras in eros venenatis, volutpat turpis nec, vestibulum nulla.`,
 	};
+	const cardQuality = {
+		icon: qualityIcon,
+		title: "CALIDAD Y DISEÑO",
+		description:
+			"Integramos la naturaleza en espacios armónicos y agradables. Utilizamos materiales y colores con vibraciones positivas para que nuestros huéspedes se sientan como en su casa.",
+	};
+	const cardBuilding = {
+		icon: buildingIcon,
+		title: "UBICADO EN AÑELO",
+		description:
+			"Añelo es la ciudad neuquina sobre la que están puestos los ojos del mundo. Es considerada la capital de Vaca Muerta, la reserva de gas y petróleo no convencional más grande la Argentina y la segunda a nivel mundial.",
+	};
 	return (
 		<>
-			<Box bg='black'>
+			<Box bg='white'>
 				<Flex alignItems='center' justifyContent='center'>
 					<Flex
 						flexDirection='column'
@@ -46,11 +49,19 @@ function ValueProposal() {
 							</Text>
 						</Box>
 						{/*  */}
-						<CTAGold text='Descargar' />
+						<CTAGold text='DESCARGAR' />
 						{/*  */}
 						<Flex>
-							<Card />
-							<Card />
+							<CustomCard
+								icon={cardQuality.icon}
+								title={cardQuality.title}
+								description={cardQuality.description}
+							/>
+							<CustomCard
+								icon={cardBuilding.icon}
+								title={cardBuilding.title}
+								description={cardBuilding.description}
+							/>
 						</Flex>
 					</Flex>
 				</Flex>
