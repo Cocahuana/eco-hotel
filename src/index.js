f9bf7da622c65ed2c61a9978d67f4d2766c010ae
Cocahuana <ezequieldominguez11@gmail.com>
2022-12-28 17:54:15 -0300

    Feat: hotjar add to index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
-const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ChakraProvider>
+			<App />
+			{(function (h, o, t, j, a, r) {
+				h.hj =
+					h.hj ||
+					function () {
+						(h.hj.q = h.hj.q || []).push(arguments);
+					};
+				h._hjSettings = { hjid: 3303532, hjsv: 6 };
+				a = o.getElementsByTagName("head")[0];
+				r = o.createElement("script");
+				r.async = 1;
+				r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+				a.appendChild(r);
+			})(
+				window,
+				document,
+				"https://static.hotjar.com/c/hotjar-",
+				".js?sv="
+			)}
+		</ChakraProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
